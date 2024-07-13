768e7234659db02f9f340a70224ec3c6c08042d6
Anushka Uniyal <124747628+anuni03@users.noreply.github.com>
2024-07-13 15:12:17 +0530

    Time: 0 ms (100%), Space: 43 MB (14.54%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-         long absN = (long) n;
-        if (n < 0) {
-            x = 1 / x;
-            absN = -absN;
+        long abs=(long)n; //imp to be in long 
+        if(n<0){
+            x=1/x;
+            abs=-abs;
         }
-        double pow = 1;
-        while (absN > 0) {
-            if ((absN %2) != 0) { //
-                pow *= x; 
-            } 
-            x *= x;
-            absN /= 2;
-        }
-        return pow;
-
+        double pow=1;
+   while(abs>0){
+    if((abs&1)!=0){ // to check odd
+        pow*=x;
+    }
+    x*=x;
+    abs>>=1; // to divide by 0
+   }
+   return pow;
     }
 }
