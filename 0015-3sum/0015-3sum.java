34b24bb97185bfa7f689a14eb3004a1f653fc1f5
Anushka Uniyal <124747628+anuni03@users.noreply.github.com>
2024-07-27 09:32:25 +0530

    Time: 28 ms (80.59%), Space: 51.6 MB (48.18%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,20 +1,30 @@
 class Solution {
+    //O(n^2 * log(unique triplets))
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> li =new HashSet<>();
-        for(int i=0;i<nums.length-2;i++){
-         Set<Integer> hs=new HashSet<>();
-         for(int j=i+1;j<nums.length;j++){
-            int third=-(nums[i]+nums[j]);
-            if(hs.contains(third)){
-                List<Integer> temp=Arrays.asList(nums[i],nums[j],third);
-                temp.sort(null);
-                li.add(temp);
-            }else{
-                hs.add(nums[j]);
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i!=0 && nums[i]==nums[i-1])continue;
+            int j=i+1;
+            int k=nums.length-1;
+            
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+if(sum<0){
+    j++;
+}else if(sum>0){
+    k--;
+}else{
+List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k]);
+ans.add(temp);
+j++;
+k--;
+
+while(j<k && nums[j]==nums[j-1])j++;
+while(j<k && nums[k]==nums[k+1])k--;
+}
             }
-         }
         }
-        List<List<Integer>> ans=new ArrayList<>(li);
-        return ans;
+return ans;
     }
 }
