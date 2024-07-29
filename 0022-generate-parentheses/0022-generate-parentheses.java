f8a60524cf537dd867bd2f654b43ddeabd5e741d
Anushka Uniyal <124747628+anuni03@users.noreply.github.com>
2024-07-29 06:14:19 +0530

    Time: 1 ms (85.31%), Space: 43.3 MB (27.61%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,17 +1,17 @@
 class Solution {
-    public void dfs(int open,int close,String s,int n,List<String> res){
+    public void dfs(int open,int close,String st,int n,List<String> str){
         if(open==close && open+close==n*2){
-            res.add(s);
-            return;
+        str.add(st);
+        return;
         }
         if(open<n)
-        dfs(open+1,close,s+'(',n,res);
+        dfs(open+1,close,st+"(",n,str);
         if(close<open)
-        dfs(open,close+1,s+')',n,res);
+        dfs(open,close+1,st+")",n,str);
     }
     public List<String> generateParenthesis(int n) {
-     List<String> res=new ArrayList<String>();
-     dfs(0,0,"",n,res);
-     return res;   
+       List<String> str=new ArrayList<>();
+       dfs(0,0,"",n,str);
+       return str; 
     }
 }
