7ade295c3379f4f293b43c11377cc64afb49a5d8
Anushka Uniyal <124747628+anuni03@users.noreply.github.com>
2024-07-29 13:43:55 +0530

    Time: 1 ms (99.98%), Space: 45.5 MB (64.1%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+      int targetIndex=nums.length-1;  
+      for(int i=nums.length-1;i>=0;i--){
+        if(targetIndex<=i+nums[i])
+        targetIndex=i;
+      }
+      return targetIndex==0;
+    }
+}
