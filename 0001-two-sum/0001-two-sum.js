2bb46ea1a13617dd002dec26a2431080e71ebcf7
Anushka Uniyal <124747628+anuni03@users.noreply.github.com>
2024-08-14 12:27:44 +0530

    Time: 56 ms (80.84%), Space: 51.7 MB (20.24%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+     var obj={} 
+   for(let i=0;i<nums.length;i++){
+    var n=nums[i]
+
+    if(obj[target-n]>=0)
+        return [obj[target-n],i];
+    else
+    obj[n]=i;
+   }
+   return obj; 
+};
